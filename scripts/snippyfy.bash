7a05c9cb2f038d9a3b48c03492d7588c0f2e59b9
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-05-14 15:29:59 +0200

    added script for creating snippy input file

---
+++ scripts/snippyfy.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get input data
+read_path=$1
+output_path=$2
+
+R1=`ls -C ${read_path}/*1.fastq.gz`
+R2=`ls -C ${read_path}/*2.fastq.gz`
+
+# Create files to merge
+for i in $R1; do
+	basename=`basename $i`
+	samplename=${basename%%_1.fastq.gz}
+	echo $samplename >> samplename.list
+done
+
+printf '%s\n' "${R1}" > R1.list
+printf '%s\n' "${R2}" > R2.list
+
+# Merge files into tab-separated file
+
+paste samplename.list R1.list R2.list > test.list
+sed -i '1i #ID	R1	R2' test.list
+
+# Cleanup
+rm samplename.list
+rm R1.list
+rm R2.list
