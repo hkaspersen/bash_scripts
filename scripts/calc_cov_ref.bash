ad255b9b7d12571843c925100bd65dc73d29fc11
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-09-18 10:05:07 +0200

    added if statement to correct for no mapped reads percent

--- scripts/calc_cov_ref.bash
+++ scripts/calc_cov_ref.bash
@@ -7,6 +7,12 @@ outputname=${outputname0##*/}
 
 zero=$(bedtools genomecov -ibam $bam -bga | awk '$4==0 {bpCountZero+=($3-$2)} {print bpCountZero}' | tail -1)
 nonzero=$(bedtools genomecov -ibam $bam -bga | awk '$4>0 {bpCountNonZero+=($3-$2)} {print bpCountNonZero}' | tail -1)
-percent=$(bc <<< "scale=6; ($nonzero / ($zero + $nonzero))*100")
+
+if [ -z "$nonzero" ]
+then
+	percent=0.0
+else
+	percent=$(bc <<< "scale=6; ($nonzero / ($zero + $nonzero))*100")
+fi
 
 echo -e "id\tpercent_mapped\tplasmid\n$outputname\t$percent\t$ref"
